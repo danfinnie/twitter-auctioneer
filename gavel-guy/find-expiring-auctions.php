891ecd92c17af997b90a2cf5c2882166645ad3f5
Dan Finnie <dan@danfinnie.com>
2013-04-06 13:19:59 -0400

    PDO DB connection for gavel guy

--- gavel-guy/find-expiring-auctions.php
+++ gavel-guy/find-expiring-auctions.php
@@ -1,3 +1,7 @@
 <?php
 
-// Some stuff
+$dbh = new PDO("mysql:host=" . getenv('AUCTIONEER_MYSQL_HOST') . ";dbname=" . getenv('AUCTIONEER_MYSQL_DB'), getenv('AUCTIONEER_MYSQL_USER'), getenv('AUCTIONEER_MYSQL_PASSWORD'));
+
+foreach($dbh->query('SELECT * from auctions') as $row) {
+    print_r($row);
+}
