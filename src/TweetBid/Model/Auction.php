2601c12a81acbc025c6dd469ecf1fe862bfa81b1
Tim Lytle <tim@timlytle.net>
2013-04-06 14:53:08 -0400

    Random stuff to annoy Dan.

---
+++ src/TweetBid/Model/Auction.php
@@ -0,0 +1,15 @@
+<?php
+namespace \TweetBid\Model;
+
+class Auction
+{
+    protected $id;
+    protected $price;
+    protected $item;
+    protected $start;
+    protected $end;
+    protected $winner;
+    protected $seller;
+    protected $active;
+    
+}
