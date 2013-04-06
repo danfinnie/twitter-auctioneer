1879fd531d5e400c261069c8314ef60e0fcef686
Dan Finnie <dan@danfinnie.com>
2013-04-06 16:04:02 -0400

    Denormalize DB a little bit, do some data storage on receiving a tweet.

--- stream-processor/server.js
+++ stream-processor/server.js
@@ -12,7 +12,8 @@ var connection = mysql.createConnection({
     host: process.env.AUCTIONEER_MYSQL_HOST,
     user: process.env.AUCTIONEER_MYSQL_USER,
     password: process.env.AUCTIONEER_MYSQL_PASSWORD,
-    database: process.env.AUCTIONEER_MYSQL_DB
+    database: process.env.AUCTIONEER_MYSQL_DB,
+    debug: true
 });
 
 /**
@@ -30,6 +31,7 @@ stream.on('data', function(data) {
     var twitter_user_name = data.user.screen_name;
     var price = /\$([\d\.]+)/.exec(data.text)[1];
     var hashtags = data.entities.hashtags.map(function(tag) { return tag.text });
+    var timestamp = Date.parse(data.created_at);
     var bid_idx = hashtags.indexOf("bid");
     var item;
 
@@ -40,10 +42,26 @@ stream.on('data', function(data) {
     else
         item = hashtags[0];
 
-    console.log({user: twitter_user_id, price: price, item: item});
-
-    connection.query('INSERT INTO bids (twitter_user_id, twitter_user_name, price, item, date) VALUES (?, ?, ?, ?, NOW())', [twitter_user_id, twitter_user_name, price, item], function(err, results) {
+    connection.query('SELECT * FROM auctions WHERE item = ? ORDER BY start_date DESC LIMIT 1', [item], function(err, results) {
         console.log(results);
+
+        if (results.length == 0)
+            return; // No such auction is running.
+
+        if (results[0].price >= price)
+            return; // Price must be monotonically increasing.
+
+        var auction_id = results[0].auction_id;
+
+        connection.query('INSERT INTO bids (twitter_user_id, twitter_user_name, price, auction_id, date) VALUES (?, ?, ?, ?, FROM_UNIXTIME(?))', [twitter_user_id, twitter_user_name, price, auction_id, timestamp/1000], function(err, results) {
+            console.log(err);
+            console.log(results);
+        });
+
+        connection.query('UPDATE auctions SET price = ?, reminders_sent = ? WHERE auction_id = ?', [price, false, auction_id], function(err, results) {
+            console.log(err);
+            console.log(results);
+        });
     });
 });
 
