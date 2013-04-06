6f3472f25a2edd414f9d43b9963faff400b022a3
Dan Finnie <dan@danfinnie.com>
2013-04-06 13:33:48 -0400

    SQL for getting expiring auctions

--- gavel-guy/find-expiring-auctions.php
+++ gavel-guy/find-expiring-auctions.php
@@ -1,7 +1,21 @@
 <?php
 
 $dbh = new PDO("mysql:host=" . getenv('AUCTIONEER_MYSQL_HOST') . ";dbname=" . getenv('AUCTIONEER_MYSQL_DB'), getenv('AUCTIONEER_MYSQL_USER'), getenv('AUCTIONEER_MYSQL_PASSWORD'));
+$last_action_timeout = 1; // 5*60*60
 
-foreach($dbh->query('SELECT * from auctions') as $row) {
+foreach($dbh->query("
+    SELECT *
+    FROM auctions
+    JOIN (SELECT max(bids.date) as last_action, auctions.auction_id
+    FROM auctions
+        JOIN bids
+        ON bids.item = auctions.item
+        AND bids.date > auctions.start_date
+        GROUP BY auctions.auction_id) maxers
+    ON maxers.auction_id = auctions.auction_id
+    WHERE last_action < NOW() - $last_action_timeout
+    AND winner_user_id IS NULL
+    ;
+") as $row) {
     print_r($row);
 }
