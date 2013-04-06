e70305db7fbebca3f3806f34440c3711874dcbcb
Dan Finnie <dan@danfinnie.com>
2013-04-06 17:48:20 -0400

    Have reminder tweets to start the count down.

--- gavel-guy/find-expiring-auctions.php
+++ gavel-guy/find-expiring-auctions.php
@@ -19,23 +19,41 @@ $twitter->addSubscriber(new OauthPlugin(array(
     'token_secret'    => getenv('AUCTIONEER_TWITTER_ACCESS_TOKEN_SECRET'),
 )));
  
+$last_bidders_stmt = $dbh->prepare("SELECT twitter_user_name FROM bids WHERE auction_id=? ORDER BY price DESC");
+$mark_sent_stmt = $dbh->prepare("UPDATE auctions SET reminders_sent=1 WHERE auction_id=?");
 
 foreach($dbh->query("
     SELECT *
     FROM auctions
     JOIN (SELECT max(bids.date) as last_action, auctions.auction_id
-    FROM auctions
+        FROM auctions
         JOIN bids
-        ON bids.item = auctions.item
-        AND bids.date > auctions.start_date
+        ON bids.auction_id = auctions.auction_id
         GROUP BY auctions.auction_id) maxers
     ON maxers.auction_id = auctions.auction_id
     WHERE last_action < NOW() - $last_action_timeout
     AND winner_user_id IS NULL
+    AND reminders_sent = 0
 ;") as $row) {
-    // print_r($row);
-    print_r ($twitter
-        ->post("statuses/update.json")
-        ->addPostFields(array("status" => "@danfinnie lolcats #$row[item]"))
-        ->send());
+    $last_bidders_stmt->bindValue(1, $row['auction_id']);
+    $last_bidders_stmt->execute();
+
+    // Do this in PHP because doing it in MySQL changes the order.
+    $last_bidders_arr = array_slice(array_unique($last_bidders_stmt->fetchAll(PDO::FETCH_COLUMN)), 1, 3);
+
+    if (count($last_bidders_arr) > 0) {
+        $last_bidders = '@' . implode(" @", $last_bidders_arr);
+        $tweet = "$last_bidders Going once on the #$row[item].  Bid now or forever hold your peace!";
+
+        echo $tweet . "\n";
+        
+        $twitter
+            ->post("statuses/update.json")
+            ->addPostFields(array("status" => $tweet))
+            ->send();
+
+        $mark_sent_stmt->execute(array($row['auction_id']));
+    } else {
+        echo "Not enough people to remind about #$row[item], no. $row[auction_id]\n";
+    }
 }
