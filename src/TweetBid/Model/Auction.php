3a9154bc7584a4389cf3bc04455eccbabeb5c51f
Tim Lytle <tim@timlytle.net>
2013-04-06 15:10:44 -0400

    Some model stuff, hope Dan doesn't mind. So not tested.

--- src/TweetBid/Model/Auction.php
+++ src/TweetBid/Model/Auction.php
@@ -1,15 +1,57 @@
 <?php
 namespace \TweetBid\Model;
-
+/**
+ * @Entity
+ * @Table(name="auctions")
+ * @author Tim Lytle <tim@timlytle.net>
+ */
 class Auction
 {
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue
+     * @var int
+     */
     protected $id;
+    
+    /**
+     * @Column(type="integer")
+     * @var int
+     */
     protected $price;
+
+    /**
+     * @Column(type="string")
+     * @var int
+     */
     protected $item;
+    
+    /**
+     * @Column(type="datetime")
+     * @var int
+     */
     protected $start;
+
+    /**
+     * @Column(type="datetime")
+     * @var int
+     */
     protected $end;
+
+    /**
+     * @ManyToOne(targetEntity="TweetBid\Model\User")
+     **/
     protected $winner;
+
+    /**
+     * @ManyToOne(targetEntity="TweetBid\Model\User")
+     **/
     protected $seller;
+
+    /**
+     * @Column(type="boolean")
+     * @var bool
+     */
     protected $active;
     
 }
