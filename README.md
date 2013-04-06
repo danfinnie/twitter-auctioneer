c923ee08f309a22df4b8ec6d26f2ecb1be0ddf7f
Dan Finnie <dan@danfinnie.com>
2013-04-06 10:16:37 -0400

    Store tweets in database

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Setup
 -----
 
 1. Install node
-2. Install user-stream with npm
+2. Install stuff with npm `npm install user-stream mysql@2.0.0-alpha7`
+3. Make a MySQL database and make a table in it:
+    `create table bids (bid_id int auto_increment primary key, price decimal(5,2), twitter_user_id varchar(255), item varchar(255) );`
 3. Copy prep_env.sh.dist to prep_env.sh and fill it out.  `source prep_env.sh`
 4. `nodejs server.js`
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var Stream = require('user-stream');
+var mysql = require('mysql');
 
 var stream = new Stream({
     consumer_key: process.env.AUCTIONEER_TWITTER_CONSUMER_KEY,
@@ -7,10 +8,29 @@ var stream = new Stream({
     access_token_secret: process.env.AUCTIONEER_TWITTER_ACCESS_TOKEN_SECRET
 });
 
+var connection = mysql.createConnection({
+    host: process.env.AUCTIONEER_MYSQL_HOST,
+    user: process.env.AUCTIONEER_MYSQL_USER,
+    password: process.env.AUCTIONEER_MYSQL_PASSWORD,
+    database: process.env.AUCTIONEER_MYSQL_DB
+});
+
 //create stream
 stream.stream();
 
 //listen stream data
-stream.on('data', function(json) {
-  console.log(json);
+stream.on('data', function(data) {
+    // Only handle tweets
+    if (!data.user || !data.text)
+        return
+
+    var twitter_user_id = data.user.id_str;
+    var price = /\$([\d\.]+)/.exec(data.text)[1];
+    var item = data.entities.hashtags[0].text;
+
+    console.log({user: twitter_user_id, price: price, item: item});
+
+    connection.query('INSERT INTO bids (twitter_user_id, price, item) VALUES (?, ?, ?)', [twitter_user_id, price, item], function(err, results) {
+        console.log(results);
+    });
 });
