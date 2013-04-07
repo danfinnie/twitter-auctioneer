b960d2d167570e7d22a06a12e5dc5ed9ebb65ac3
Tim Lytle <tim@timlytle.net>
2013-04-07 14:55:57 -0400

    Might work.

--- src/TweetBid/Service/Gavel.php
+++ src/TweetBid/Service/Gavel.php
@@ -41,21 +41,50 @@ class Gavel
 
     private function handleAnnouncedAuction($auction) {
         $lastBid = $auction->getLastBid();
-        if ($this->bidIsPastTimeout($lastBid)) {
-            $bidders = $auction->getBidders();
-            //$last_bidder_idx = array_search($lastBid->getUser(), $bidders);
-            //unset($bidders[$last_bidder_idx]);
-            $tweet_str = "";
-            foreach($bidders as $bidder)
-                $tweet_str .= '@' . $bidder->getName() . ' ';
-            $tweet_str .= " Going once on the " . $auction->getItem();
-            $this->tweeter->tweet($tweet_str);
-            $auction->advanceState();
+        //no one bid
+        if(!$lastBid){
+            return;
         }
+        
+        $this->tweeter->tweet("Now " . $lastBid->getUser()->getName() . " is the high bidder for #" . $auction->getItem() . " at $" . $lastBid->getAmount()/100);
+        $auction->advanceState();
     }
 
     private function handleContestedAuction($auction) {
-
+        $lastBid = $auction->getLastBid();
+        
+        if($lastBid->getTimestamp()->add($this->auctionTimeout)->add($this->auctionTimeout) < new \DateTime()){
+            //close that puppy and tweet out the winner
+            $auction->close();
+            
+            //also, charge the sucker
+            $account = \Balanced\Account::get($lastBid->getUser()->getAccount());
+            $account->debit($lastBid->getAmount());
+            
+            $this->tweeter->tweet("Sold! @" . $lastBid->getUser()->getName() . " won #" . $auction->getItem());
+            return;
+        }
+        
+        if ($lastBid->getTimestamp()->add($this->auctionTimeout) < new \DateTime()) {
+            $bidders = $auction->getBidders();
+            
+            //$last_bidder_idx = array_search($lastBid->getUser(), $bidders);
+            //unset($bidders[$last_bidder_idx]);
+            $tweet_str = "Going once #" . $auction->getItem();
+            foreach($bidders as $bidder){
+                if($bidder == $lastBid->getUser()){
+                    continue;
+                }
+                
+                if(strlen($tweet_str) + 2 + strlen($bidder->getName())){
+                    continue;
+                }
+                
+                $tweet_str .= ' @' . $bidder->getName();
+            }
+            $this->tweeter->tweet($tweet_str);
+            $auction->advanceState();
+        }        
     }
 
     private function handleWonAuction($auction) {
@@ -63,6 +92,6 @@ class Gavel
     }
 
     private function bidIsPastTimeout($bid) {
-       return $bid->getTimestamp()->add($this->auctionTimeout()) < new \DateTime();
+       return $bid->getTimestamp()->add($this->auctionTimeout) < new \DateTime();
     }
 }
